8cc40ef45501c12593c722d4d6604679d70e8441
Innocent Asiimwe <asiimwe@outlook.com>
2017-04-18 12:32:17 +0300

    "updating primes.py , raising exceptions"

--- primes.py
+++ primes.py
@@ -1,1 +1,18 @@
- def primes(number):
+def primes(upperlimit):
+    prime_numbers = []
+    non_primes = []
+
+    if isinstance(upperlimit, int):
+        if upperlimit >= 2:
+            for number in range(2, upperlimit+1):
+                if number not in non_primes:
+                    prime_numbers.append(number)
+
+                    for i in range ( number * number, upperlimit +1, number ):
+                        non_primes.append(i)
+        else:
+            raise ValueError("limit should be greater than 2 ")
+    else:
+        raise(TypeError, "limit should be  interger")
+
+    return prime_numbers
